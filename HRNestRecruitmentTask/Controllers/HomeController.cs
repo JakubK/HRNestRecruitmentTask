4733d11f131b3ca43c2055d03fdcadb9a9c543d8
JakubK <jakubkuniszewski1@gmail.com>
2019-08-06 15:13:57 +0200

    Inject IDatabaseSeeder to HomeController and add call to it's Seed method

--- HRNestRecruitmentTask/Controllers/HomeController.cs
+++ HRNestRecruitmentTask/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿using HRNestRecruitmentTask.Context;
 using HRNestRecruitmentTask.Models;
 using HRNestRecruitmentTask.Repository;
+using HRNestRecruitmentTask.Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,14 +13,15 @@ namespace HRNestRecruitmentTask.Controllers
     public class HomeController : Controller
     {
         IRepository _repository;
-        public HomeController(IRepository repository)
+        public HomeController(IRepository repository, IDatabaseSeeder seeder)
         {
             _repository = repository;
+            seeder.Seed();
         }
         // GET: Home
         public ActionResult Index()
         {
-            return View(_repository.GetContacts());
+            return View(_repository.GetContacts(););
         }
     }
 }
