cf25786460b9f17e6deb230defd77a40cdff86d9
JakubK <jakubkuniszewski1@gmail.com>
2019-08-06 14:20:58 +0200

    Create IRepository with it's implementation containing method that returns all Contact data

---
+++ HRNestRecruitmentTask/Repository/IRepository.cs
@@ -0,0 +1,14 @@
+﻿using HRNestRecruitmentTask.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRNestRecruitmentTask.Repository
+{
+    public interface IRepository
+    {
+        IEnumerable<Contact> GetContacts();
+    }
+}
---
+++ HRNestRecruitmentTask/Repository/Repository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using HRNestRecruitmentTask.Context;
+using HRNestRecruitmentTask.Models;
+
+namespace HRNestRecruitmentTask.Repository
+{
+    public class Repository : IRepository
+    {
+        ContactContext context;
+        
+        public Repository()
+        {
+           context = new ContactContext();
+        }
+
+        public IEnumerable<Contact> GetContacts()
+        {
+           return context.Contacts;
+        }
+    }
+}
