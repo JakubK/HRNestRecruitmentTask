6e66c32454fe2081a4f1c5be0ce55830368f0f11
JakubK <jakubkuniszewski1@gmail.com>
2019-08-06 19:10:04 +0200

    Create repository for User management

---
+++ HRNestRecruitmentTask/Repository/UserRepository.cs
@@ -0,0 +1,36 @@
+﻿using HRNestRecruitmentTask.Context;
+using HRNestRecruitmentTask.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HRNestRecruitmentTask.Repository
+{
+    public class UserRepository : IRepository<User>
+    {
+        ContactContext context;
+        public UserRepository(ContactContext ctx)
+        {
+            context = ctx;
+        }
+
+
+        public void Add(User user)
+        {
+            context.Users.Add(user);
+            context.SaveChanges();
+        }
+
+        public void AddRange(IEnumerable<User> users)
+        {
+            context.Users.AddRange(users);
+            context.SaveChanges();
+        }
+
+        public IEnumerable<User> GetAll()
+        {
+            return context.Users;
+        }
+    }
+}
