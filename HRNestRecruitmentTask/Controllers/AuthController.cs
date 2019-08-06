04fb606a03369e8ac92edab94908e148ca1a3b5e
JakubK <jakubkuniszewski1@gmail.com>
2019-08-06 19:01:51 +0200

    Create AuthController with Register and Login method

---
+++ HRNestRecruitmentTask/Controllers/AuthController.cs
@@ -0,0 +1,36 @@
+﻿using HRNestRecruitmentTask.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HRNestRecruitmentTask.Controllers
+{
+    public class AuthController : Controller
+    {
+        // GET: Auth
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Register(RegisterViewModel register)
+        {
+            if(ModelState.IsValid && register.Password == register.PasswordRepeat)
+            {
+                System.Diagnostics.Debug.WriteLine("Corret reigster");
+
+                return RedirectToAction("Login");
+            }
+
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+    }
+}
---
+++ HRNestRecruitmentTask/Models/RegisterViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace HRNestRecruitmentTask.Models
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+
+        [Required]
+        [Compare("Password")]
+        public string PasswordRepeat { get; set; }
+    }
+}
