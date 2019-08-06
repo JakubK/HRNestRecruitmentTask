a2476ec7dbe84d4299b18c1a53e3f8f649b3907a
JakubK <jakubkuniszewski1@gmail.com>
2019-08-06 14:04:56 +0200

    Create ContactContext and initialize the Database

---
+++ HRNestRecruitmentTask/Context/ContactContext.cs
@@ -0,0 +1,19 @@
+﻿using HRNestRecruitmentTask.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace HRNestRecruitmentTask.Context
+{
+    public class ContactContext : DbContext
+    {
+        public ContactContext() : base()
+        {
+
+        }
+
+        public DbSet<Contact> Contacts { get; set; }
+    }
+}
