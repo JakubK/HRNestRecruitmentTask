c926c4d2738489cc87905f517a9d3ca295991128
JakubK <jakubkuniszewski1@gmail.com>
2019-08-06 19:05:59 +0200

    Update error messages from RegisterViewModel and add Regular Expression for matching strong passwords

--- HRNestRecruitmentTask/Models/RegisterViewModel.cs
+++ HRNestRecruitmentTask/Models/RegisterViewModel.cs
@@ -8,15 +8,16 @@ namespace HRNestRecruitmentTask.Models
 {
     public class RegisterViewModel
     {
-        [Required]
-        [EmailAddress]
+        [Required(ErrorMessage = "This field is required")]
+        [EmailAddress(ErrorMessage ="The given string is not a valid email address")]
         public string Email { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "This field is required")]
+        [RegularExpression("^(?=.*[A-Z].*[A-Z])(?=.*[!@#$&*])(?=.*[0-9].*[0-9])(?=.*[a-z].*[a-z].*[a-z]).{8}$", ErrorMessage = "The password is not strong enough")]
         public string Password { get; set; }
 
-        [Required]
-        [Compare("Password")]
+        [Required(ErrorMessage = "This field is required")]
+        [Compare("Password", ErrorMessage = "Given passwords are not the same")]
         public string PasswordRepeat { get; set; }
     }
 }
