674a06beed424dcdbeb7db60807ca0b0c0a62766
JakubK <jakubkuniszewski1@gmail.com>
2019-08-06 14:04:24 +0200

    Create Contact class with Email marked as Key in the Database

---
+++ HRNestRecruitmentTask/Models/Contact.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace HRNestRecruitmentTask.Models
+{
+    public class Contact
+    {
+        [Key]
+        public string Email { get; set; }
+  
+
+        public string Name { get; set; }
+        public string Surname { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        public DateTime BirthDate { get; set; }
+    }
+}
