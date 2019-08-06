b61091fa3ea850b05e3547fdbf12e7d4d1d176e6
JakubK <jakubkuniszewski1@gmail.com>
2019-08-06 19:31:14 +0200

    Add LoginViewModel and Login view

---
+++ HRNestRecruitmentTask/Models/LoginViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace HRNestRecruitmentTask.Models
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage = "This field is required")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "This field is required")]
+        public string Password { get; set; }
+    }
+}
