511636f3ea80ee26bae1527ff5e41af62aefc2fe
JakubK <jakubkuniszewski1@gmail.com>
2019-08-08 14:35:47 +0200

    Create SaltHelper static class for salt generation

---
+++ HRNestRecruitmentTask/Helpers/SaltHelper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Web;
+
+namespace HRNestRecruitmentTask.Helpers
+{
+    public static class SaltHelper
+    {
+        public static string GenerateSalt()
+        {
+            byte[] bytes = new byte[128 / 8];
+            using (var keyGenerator = RandomNumberGenerator.Create())
+            {
+                keyGenerator.GetBytes(bytes);
+                return BitConverter.ToString(bytes);
+            }
+        }
+    }
+}
