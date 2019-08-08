85a4353de024639be02b5ad0be22dec43cc6a6f1
JakubK <jakubkuniszewski1@gmail.com>
2019-08-08 09:33:58 +0200

    Create ContactController with Index View and Delete method

---
+++ HRNestRecruitmentTask/Controllers/ContactController.cs
@@ -0,0 +1,30 @@
+﻿using HRNestRecruitmentTask.Models;
+using HRNestRecruitmentTask.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HRNestRecruitmentTask.Controllers
+{
+    public class ContactController : Controller
+    {
+        IRepository<Contact> _repository;
+        public ContactController(IRepository<Contact> repo)
+        {
+            _repository = repo;
+        }
+        // GET: Contact
+        public ActionResult Index()
+        {
+            return View(_repository.GetAll());
+        }
+
+        public ActionResult Delete(string email)
+        {
+            _repository.Delete(_repository.GetByEmail(email));
+            return RedirectToAction("Index");
+        }
+    }
+}
