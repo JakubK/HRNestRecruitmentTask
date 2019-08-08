24f30fb787d334209d66fe7be428392b29661dd2
JakubK <jakubkuniszewski1@gmail.com>
2019-08-08 15:22:05 +0200

    Add some DataAnnotations to the ContactForm

--- HRNestRecruitmentTask/Models/ContactForm.cs
+++ HRNestRecruitmentTask/Models/ContactForm.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,11 +8,13 @@ namespace HRNestRecruitmentTask.Models
 {
     public class ContactForm
     {
+        [EmailAddress]
         public string Email { get; set; }
 
         public string Name { get; set; }
         public string Surname { get; set; }
 
+        [Phone]
         public string PhoneNumber { get; set; }
 
         public DateTime BirthDate { get; set; }
