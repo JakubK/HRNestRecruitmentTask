cae9f72249bdf0edf2a6324790ee3b0919311cb9
JakubK <jakubkuniszewski1@gmail.com>
2019-08-08 14:36:31 +0200

    Create UserConext

---
+++ HRNestRecruitmentTask/Context/UserContext.cs
@@ -0,0 +1,19 @@
+﻿using HRNestRecruitmentTask.Models;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace HRNestRecruitmentTask.Context
+{
+    public class UserContext : IdentityDbContext<ApplicationUser>
+    {
+        public UserContext() : base("xsxsxsxs")
+        {
+            Configuration.ProxyCreationEnabled = false;
+        }
+
+    }
+}
