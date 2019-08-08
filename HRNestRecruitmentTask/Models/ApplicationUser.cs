d2bf5ae18705efa4640d3d814aca81626a6a6ccf
JakubK <jakubkuniszewski1@gmail.com>
2019-08-08 14:36:59 +0200

    Substitute User class with ApplicationUser

--- HRNestRecruitmentTask/Models/ApplicationUser.cs
+++ HRNestRecruitmentTask/Models/ApplicationUser.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
@@ -6,10 +7,8 @@ using System.Web;
 
 namespace HRNestRecruitmentTask.Models
 {
-    public class User
+    public class ApplicationUser : IdentityUser
     {
-        [Key]
-        public string Email { get; set; }
-        public string PasswordHash { get; set; }
+        public string Salt { get; set; }
     }
 }
