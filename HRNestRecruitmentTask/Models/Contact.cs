7cbc84ab89607c7330e9fe8c3a000db7557d559b
JakubK <jakubkuniszewski1@gmail.com>
2019-08-08 10:25:01 +0200

    Add ID field to Contact and make it a key instead of an Email

--- HRNestRecruitmentTask/Models/Contact.cs
+++ HRNestRecruitmentTask/Models/Contact.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
@@ -9,6 +10,9 @@ namespace HRNestRecruitmentTask.Models
     public class Contact
     {
         [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ID { get; set; }
+
         public string Email { get; set; }
   
 
